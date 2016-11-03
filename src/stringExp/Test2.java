1960653ba6565f365b14cfeae97604da2dde8e3c
andrey.demchuk <andreichdem@gmail.com>
2016-11-03 13:41:36 +0200

    init commit

---
+++ src/stringExp/Test2.java
@@ -0,0 +1,6 @@
+package stringExp;
+public class Test2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/tutorial/Test4.java
@@ -0,0 +1,17 @@
+package tutorial;
+
+import java.util.Scanner;
+
+public class Test4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = 0;
+        do {
+            N = sc.nextInt();
+        } while (N < 2 && N > 20);
+        for (int i = 1; i <= 10; i++) {
+            int result = i * N;
+            System.out.println(N + " x " + i + " = " + result);
+        }
+    }
+}
