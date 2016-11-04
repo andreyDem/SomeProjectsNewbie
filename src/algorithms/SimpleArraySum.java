739f7541e3e74b85b861a1c647dd585c064bb191
andrey.demchuk <andreichdem@gmail.com>
2016-11-04 14:52:41 +0200

    2 task complete

---
+++ src/algorithms/SimpleArraySum.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SimpleArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int arr[] = new int[N];
+        int i = 0;
+        while (N!=0){
+            arr[i] = scanner.nextInt();
+            N--;
+            i++;
+        }
+        int sum = 0;
+        for (int j = 0; j < arr.length; j++) {
+            sum = sum + arr[j];
+        }
+        System.out.println(sum);
+        //System.out.println(Arrays.toString(arr));
+    }
+}
--- src/algorithms/Test.java
+++ src/algorithms/Test.java
@@ -1,8 +1,6 @@
 package algorithms;
-
-
 public class Test {
     public static void main(String[] args) {
-
+        
     }
 }
