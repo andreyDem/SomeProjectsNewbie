59e8e2da5851fb70809f3605e6c4b4de8936d022
andrey.demchuk <andreichdem@gmail.com>
2016-12-02 14:40:40 +0200

    +1 Day started

---
+++ src/coding30Days/DayRecursion.java
@@ -0,0 +1,24 @@
+package coding30Days;
+
+import java.util.Scanner;
+
+public class DayRecursion {
+    public static void main(String[] args) {
+        //factorial
+        Scanner sc = new Scanner(System.in);
+
+        int N;
+        do {
+            N = sc.nextInt();
+        } while (N > 12 && N < 2);
+
+        System.out.println(factorial(N));
+    }
+
+    private static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n-1);
+    }
+}
