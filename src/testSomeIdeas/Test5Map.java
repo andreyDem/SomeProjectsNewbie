4d8ffb14e4d871467b53ca4f135f25e41a53859f
andrey.demchuk <andreichdem@gmail.com>
2016-12-19 14:37:41 +0200

    Map and HashMap test

---
+++ src/testSomeIdeas/Test5Map.java
@@ -0,0 +1,10 @@
+package testSomeIdeas;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Test5Map {
+    public static void main(String[] args) {
+        Map<String, String> map = new TreeMap<>();
+    }
+}
